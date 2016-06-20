4e643ea675e4d569367b614067dfa18c42b4da52
Alex Navasardyan <alex.navasardyan@gmail.com>
2016-06-20 14:09:56 -0700

    sttuff

--- tests/acceptance/brocfile-smoke-test-slow.js
+++ tests/acceptance/brocfile-smoke-test-slow.js
@@ -391,12 +391,12 @@ describe('Acceptance: brocfile-smoke-test', function() {
       });
   });
 
-  it('multiple paths can be CSS preprocessed', function() {
+  it.only('multiple paths can be CSS preprocessed', function() {
     return copyFixtureFiles('brocfile-tests/multiple-sass-files')
       .then(function() {
         var packageJsonPath = path.join(__dirname, '..', '..', 'tmp', appName, 'package.json');
         var packageJson = fs.readJsonSync(packageJsonPath);
-        packageJson.devDependencies['broccoli-sass'] = 'latest';
+        packageJson.devDependencies['ember-cli-sass'] = 'latest';
 
         return fs.writeJsonSync(packageJsonPath, packageJson);
       })
